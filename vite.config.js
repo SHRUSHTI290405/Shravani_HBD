00ea962b814b14cc45c5ad8f294dec57990d95b9
SHRUSHTI290405 <shrushtiwarade29@gmail.com>
2026-03-04 21:15:41 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Replace this with your exact repo name from Phase 1
+  base: 'ShravaniHBD', // Replace this with your exact repo name from Phase 1
 })
